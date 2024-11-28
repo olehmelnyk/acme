1aada0bd5d012420d8a27b9abaddc379707a56bf
Oleh Melnyk <oleh.melnyk@tui.com>
2024-11-28 20:10:02 +0200

    chore: rename apps/next to apps/web

--- apps/web-e2e/playwright.config.ts
+++ apps/web-e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'bun nx run next:serve-static',
+    command: 'bun nx run web:serve-static',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/next',
+  cacheDir: '../../node_modules/.vite/apps/web',
 
   plugins: [react(), nxViteTsPaths()],
 
@@ -23,7 +23,7 @@ export default defineConfig({
     include: ['app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: {
-      reportsDirectory: '../../coverage/apps/next',
+      reportsDirectory: '../../coverage/apps/web',
       provider: 'v8'
     }
   }
