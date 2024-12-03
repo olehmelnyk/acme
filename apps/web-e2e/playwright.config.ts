5e4cd70d374b55a15a7b83ff0984a30b45040465
Oleh Melnyk <oleh.melnyk@tui.com>
2024-12-03 15:34:22 +0200

    test(e2e): Improve e2e

--- apps/web-e2e/playwright.config.ts
+++ apps/web-e2e/playwright.config.ts
@@ -16,6 +16,19 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:3000';
  */
 export default defineConfig({
   ...nxE2EPreset(__filename, { testDir: './e2e' }),
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: `${workspaceRoot}/.output/apps/web-e2e/playwright/html-report` }],
+    ['junit', { outputFile: `${workspaceRoot}/.output/apps/web-e2e/playwright/results.xml` }]
+  ],
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env['CI'],
+  /* Retry on CI only */
+  retries: process.env['CI'] ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env['CI'] ? 1 : undefined,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL,
